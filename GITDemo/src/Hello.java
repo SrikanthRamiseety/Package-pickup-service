61e3ea2fec1fc6c40aa27a220aafe7684587dfbf
SrikanthRamiseety <ramisetty.srikanth2@gmail.com>
2015-01-22 12:56:32 -0800

    First Git Hub Demo

---
+++ GITDemo/src/Hello.java
@@ -0,0 +1,17 @@
+
+/**
+ * This is the class for testing purpose only.
+ * 
+ * @author Lakshman Sir
+ * @since 16th Jan 2015
+ */
+public class Hello {
+
+	/**
+	 * This is the main method.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to GitHub..");
+	}
+}
